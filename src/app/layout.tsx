cdfd8fc2b9fa065fe82d08afe29f8fde584599c1
Andres <andrebrayan74@gmail.com>
2022-11-04 19:14:33 -0500

    chore: add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,26 @@
 import { ReactNode } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import store from "@store";
 
 import "@styles/globals.css";
 
 const RootLayout = ({ children }: TRootLayout) => {
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { refetchOnWindowFocus: false } },
+  });
+
   return (
     <html>
       <head></head>
-      <body>{children}</body>
+      <body>
+        <ReduxProvider store={store}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </ReduxProvider>
+      </body>
     </html>
   );
 };
